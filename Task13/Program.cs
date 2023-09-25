a4b02183d5100b337f00d07ed650c0ad33a5c68d
Vladimir V <veselov2100@mail.ru>
2023-09-25 16:57:22 +0300

    Added Homework 2

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int GetThirdDigit(int num)
+{
+    num = Math.Abs(num);
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 && number > -100) Console.WriteLine($"{number} -> Третьей цифры нет");
+else Console.WriteLine($"{number} -> {GetThirdDigit(number)}");
--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool CheckWeekend(int day)
+{
+    if (day == 6 || day == 7) return true;
+    else return false;
+}
+
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7) Console.WriteLine("Некорретный ввод");
+else if (CheckWeekend(day)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
