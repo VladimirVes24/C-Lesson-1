77a3b590abb6bff3422060faf00d11575311501e
Vladimir V <veselov2100@mail.ru>
2023-10-02 21:02:09 +0300

    Added HW4

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetDigitsSum(int num)
+{
+    int sum = 0;
+    num = Math.Abs(num);
+    while (num > 9)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    if (num < 9) sum += num;
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = GetDigitsSum(number);
+Console.WriteLine($"Сумма цифр: {res}");
---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length]; 
+    for (int i = 0; i < length; ++i)
+    {
+        Console.Write($"Введите {i} элемент: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        Console.Write($"{arr[i]}");
+        if (i < (arr.Length - 1)) Console.Write(", ");
+        else Console.WriteLine("]");
+    }
+    
+}
+
+int[] ar = CreateArray(8);
+PrintArray(ar);
