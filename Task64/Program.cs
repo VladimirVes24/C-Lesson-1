5ce069e4f298c69f0f3033b288cb05e5a1e6c9e2
Vladimir V <veselov2100@mail.ru>
2023-10-23 19:14:08 +0300

    HW9 complete

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNaturalNumbersTo(int number)
+{
+    Console.Write($"{number} ");
+    if (number == 1) return;
+    PrintNaturalNumbersTo(number - 1);
+}
+
+Console.Write("Введите натуральное число N: ");
+int naturalNumber = Convert.ToInt32(Console.ReadLine());
+if (naturalNumber < 1) Console.WriteLine("Некорректный ввод");
+else PrintNaturalNumbersTo(naturalNumber);
---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int CountSumOfNaturalNumBetween(int min, int max)
+{
+    int currentSum = max;
+    if (max > min)
+    {
+        currentSum += CountSumOfNaturalNumBetween(min, max-1);
+    }
+    return currentSum;
+}
+
+Console.Write("Введите натуральное число M: ");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int maximum = Convert.ToInt32(Console.ReadLine());
+if (maximum < minimum) Console.WriteLine("Некорректный ввод");
+else 
+{
+    int sum = CountSumOfNaturalNumBetween(minimum, maximum);
+    Console.WriteLine($"Сумма натуральных чисел в заданном промежутке: {sum}");
+}
